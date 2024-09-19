34cb37c8afa1d720961ac067addb1966670724bf
unknown <dietdoeur@gmail.com>
2024-09-19 16:32:02 +0700

    create code slider

--- pages/slider/index.vue
+++ pages/slider/index.vue
@@ -1,82 +1,91 @@
 <template>
-     <div class="slider">
-         <div class="list">
-            <div class="list">
-                <div class="item">
-                    <img src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q" alt="">
-                    <div class="content">
-                        <div class="title">MAGIC SLIDER</div>
-                        <div class="type">FLOWER</div>
-                        <div class="description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, asperiores officiis illum harum culpa maxime minima dolor quasi ab voluptas architecto ullam explicabo omnis placeat quis nihil beatae eos vero?
-                        </div>
-                        <div class="button">
-                            <button>SEE MORE</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q" alt="">
-                    <div class="content">
-                        <div class="title">MAGIC SLIDER</div>
-                        <div class="type">FLOWER</div>
-                        <div class="description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, asperiores officiis illum harum culpa maxime minima dolor quasi ab voluptas architecto ullam explicabo omnis placeat quis nihil beatae eos vero?
-                        </div>
-                        <div class="button">
-                            <button>SEE MORE</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q" alt="">
-                    <div class="content">
-                        <div class="title">MAGIC SLIDER</div>
-                        <div class="type">FLOWER</div>
-                        <div class="description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, asperiores officiis illum harum culpa maxime minima dolor quasi ab voluptas architecto ullam explicabo omnis placeat quis nihil beatae eos vero?
-                        </div>
-                        <div class="button">
-                            <button>SEE MORE</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q" alt="">
-                    <div class="content">
-                        <div class="title">MAGIC SLIDER</div>
-                        <div class="type">FLOWER</div>
-                        <div class="description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, asperiores officiis illum harum culpa maxime minima dolor quasi ab voluptas architecto ullam explicabo omnis placeat quis nihil beatae eos vero?
-                        </div>
-                        <div class="button">
-                            <button>SEE MORE</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-         </div>
-         <div class="thumbnail">
-                <div class="item">
-                    <img src="https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTHYKjYLD2Dhiy86TTKYv-6TDux84y_Z__45XNhr1Y8XMFZWysY60LbXtS7qH6WRIMhQcBgEh4VB7flwnITfmkxfB-xCZ5f2nUlLsP5qg" alt="">
-                </div>
-                <div class="item">
-                    <img src="https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTHYKjYLD2Dhiy86TTKYv-6TDux84y_Z__45XNhr1Y8XMFZWysY60LbXtS7qH6WRIMhQcBgEh4VB7flwnITfmkxfB-xCZ5f2nUlLsP5qg" alt="">
-                </div>
-                <div class="item">
-                    <img src="https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTHYKjYLD2Dhiy86TTKYv-6TDux84y_Z__45XNhr1Y8XMFZWysY60LbXtS7qH6WRIMhQcBgEh4VB7flwnITfmkxfB-xCZ5f2nUlLsP5qg" alt="">
-                </div>
-                <div class="item">
-                    <img src="https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTHYKjYLD2Dhiy86TTKYv-6TDux84y_Z__45XNhr1Y8XMFZWysY60LbXtS7qH6WRIMhQcBgEh4VB7flwnITfmkxfB-xCZ5f2nUlLsP5qg" alt="">
-                </div>
-         </div>
-         <div class="nextPrevArrows">
-            <button class="prev"></button>
-            <button class="next"></button>
-         </div>
-     </div>
+  <div class="slider">
+    <div class="list">
+      <div class="item" v-for="(item, index) in items" :key="index">
+        <img :src="item.img" alt="">
+        <div class="content">
+          <div class="title">{{ item.title }}</div>
+          <div class="type">{{ item.type }}</div>
+          <div class="description">{{ item.description }}</div>
+          <div class="button">
+            <button>SEE MORE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="thumbnail">
+      <div class="item" v-for="(thumbnail, index) in thumbnails" :key="index">
+        <img :src="thumbnail.img" alt="">
+      </div>
+    </div>
+    <div class="nextPrevArrows">
+      <button class="prev" @click="moveSlider('prev')">Prev</button>
+      <button class="next" @click="moveSlider('next')">Next</button>
+    </div>
+  </div>
 </template>
 
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const items = ref([
+  {
+    img: 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q',
+    title: 'MAGIC SLIDER',
+    type: 'FLOWER',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+  },
+  {
+    img: 'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTHYKjYLD2Dhiy86TTKYv-6TDux84y_Z__45XNhr1Y8XMFZWysY60LbXtS7qH6WRIMhQcBgEh4VB7flwnITfmkxfB-xCZ5f2nUlLsP5qg',
+    title: 'MAGIC SLIDER',
+    type: 'FLOWER',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...',
+  },
+])
+
+const thumbnails = ref([
+  { img: 'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTHYKjYLD2Dhiy86TTKYv-6TDux84y_Z__45XNhr1Y8XMFZWysY60LbXtS7qH6WRIMhQcBgEh4VB7flwnITfmkxfB-xCZ5f2nUlLsP5qg' },
+  { img: 'https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTGwVa2x0c4RVl-4wQIqXYT9a9uu3REVmnF1Wy5Uhjfwayh_q-MyqW0gwBG299zRw-crdW8yGT4gs_LqsZJW0mvNXMuggLEnSGoMYPp8Q' },
+  { img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcToHP5KjxRNU7vLmbDDFtEY2AABbEYx4phBBInV9lsW_q-rbJvVpxkOt4hdPLrVr2v4KUCF1jdEcm-D8iObMlQoLyULHKRgxg' },
+  { img: 'https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcShElSZMhpXoMzx5v6xvJ3vNf3EjhW7JAWnhkKoWpmQ0c_HfqgQpg83v94HQbKWSVxhPrkE_93AZpJ6spuBbHr0bfirU9nONow' }
+]);
+
+
+const slider = ref<HTMLElement | null>(null)
+const sliderList = ref<HTMLElement | null>(null)
+const thumbnail = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  slider.value = document.querySelector('.slider')
+  sliderList.value = slider.value?.querySelector('.list') as HTMLElement
+  thumbnail.value = document.querySelector('.thumbnail') as HTMLElement
+})
+
+function moveSlider(direction: string) {
+  const sliderItems = sliderList.value?.querySelectorAll('.item')
+  const thumbnailItems = thumbnail.value?.querySelectorAll('.item')
+
+  if (sliderItems && thumbnailItems) {
+    if (direction === 'next') {
+      sliderList.value?.appendChild(sliderItems[0])
+      thumbnail.value?.appendChild(thumbnailItems[0])
+      slider.value?.classList.add('next')
+    } else {
+      sliderList.value?.prepend(sliderItems[sliderItems.length - 1])
+      thumbnail.value?.prepend(thumbnailItems[thumbnailItems.length - 1])
+      slider.value?.classList.add('prev')
+    }
+
+    slider.value?.addEventListener(
+      'animationend',
+      () => {
+        slider.value?.classList.remove(direction)
+      },
+      { once: true }
+    )
+  }
+}
+</script>
 <style scoped>
 * {
     margin:0;
@@ -184,4 +193,148 @@ a {
     gap:10px;
     align-items:center;
 }
+.nextPrevArrows button {
+    width:40px;
+    height:40px;
+    border-radius:50%;
+    background-color:white;
+    border:none;
+    font-family:monospace;
+    font-weight:bold;
+    cursor: pointer;
+    transition:0.5s;
+}
+
+.nextPrevArrows button:hover {
+    background-color:yellow;
+}
+
+/* animation part */
+.slider .list .item:nth-child(1) {
+    z-index:1;
+}
+.slider .list .item:nth-child(1) .content .title,
+.slider .list .item:nth-child(1) .content .type,
+.slider .list .item:nth-child(1)  .content .description,
+.slider .list .item:nth-child(1)  .content .button {
+    transform: translateY(50px);
+    filter:blur(20px);
+    opacity:0;
+    animation: showContent 0.5s 1s linear 1 forwards;
+}
+
+@keyframes showContent {
+    to {
+        transform: translateY(0px);
+        filter:blur(00px);
+        opacity:1;
+    }
+}
+.slider .list .item:nth-child(1) .content .title {
+    animation-delay:0.4s;
+}
+.slider .list .item:nth-child(1) .content .type {
+    animation-delay:0.6s;
+}
+.slider .list .item:nth-child(1) .content .description {
+    animation-delay:0.8s;
+}
+.slider .list .item:nth-child(1) .content .button {
+    animation-delay:1s;
+}
+
+/* Animation for next button click */
+.slider.next .list .item:nth-child(1) img {
+    width:150px;
+    height:220px;
+    position:absolute;
+    bottom:50px;
+    left: 50%;
+    border-radius:30px;
+    animation: showImage 0.5s 1s linear 1 forwards;
+}
+@keyframes showImage {
+    to {
+        bottom:0;
+        left:0;
+        width:100%;
+        height:100%;
+        border-radius:0;
+    }
+}
+
+.slider.next .thumbnail .item:nth-child(1) {
+    overflow:hidden;
+    animation:showThumbnail 0.5s linear 1 forwards;
+}
+
+.slider.prev .list .item img {
+    z-index: 100;
+}
+@keyframes showThumbnail {
+    from {
+        width:0;
+        opacity:0;
+    }
+}
+
+.slider.next .thumbnail {
+    animation:effectNext .5s linear 1 forwards;
+}
+@keyframes effectNext {
+    from {
+        transform: translateX(150px);
+    }
+}
+
+/* Animation for prev button click  */
+.slider.prev .list .item:nth-child(2) {
+    z-index:2;
+}
+.slider.prev .list .item:nth-child(2) img {
+    position:absolute;
+    bottom:0;
+    left:0;
+    animation:outFrame 0.5s linear 1 forwards;
+}
+
+@keyframes outFrame {
+    to {
+        width:150px;
+        height:220px;
+        bottom:50px;
+        left:50%;
+        border-radius:20px;
+    }
+}
+
+.slider.prev .thumbnail .item:nth-child(1) {
+    overflow:hidden;
+    opacity:0;
+    animation:showThumbnail .5s linear 1 forwards;
+}
+
+
+.slider.prev .item:nth-child(1) .content .title,
+.slider.prev .item:nth-child(1) .content .type,
+.slider.prev .item:nth-child(1)  .content .description,
+.slider.prev .item:nth-child(1)  .content .button {
+    animation:contentOut 0.5s 1s linear 1 forwards;
+}
+@keyframes contentOut {
+    to {
+        transform:translateY(-150px);
+        filter:blur(20px);
+        opacity:0;
+    }
+}
+@media screen and (max-width: 678px) {
+    .slider .list .item .content {
+        padding-right:0;
+    }
+    .slider .list .item .content .title {
+        font-size:30px;
+    }
+}
+
 </style>
---
+++ pages/slider1/index.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="main">
+        <div class="container">
+             <div class="slider-wrapper">
+                <button id="prev-slide" class="slider-button material-symbols-outlined">chevron_left</button>
+                <div class="image-list">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-1" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-2" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-3" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-4" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-5" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-6" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-7" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-8" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-9" class="image-item">
+                    <img src="https://cdn.britannica.com/49/94449-050-ECB0E7C2/Angkor-Wat-temple-complex-Camb.jpg" alt="img-10" class="image-item">
+                </div>
+                <button id="next-slide" class="slider-button material-symbols-outlined">chevron_right</button>
+             </div>
+             <div class="slider-scrollbar">
+                 <div class="scrollbar-track">
+                     <div class="scrollbar-thumb"></div>
+                 </div>
+             </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+.main {
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    min-height:100vh;
+    background:teal;
+}
+.container {
+    max-width: 1200px;
+    width:95%;
+}
+
+.slider-wrapper .slider-button {
+    position:absolute;
+    outline:none;
+    top:50%;
+    border:none;
+    height:50px;
+    width:50px;
+    color:#fff;
+    background: #000;
+    font-size:2.2rem;
+    cursor: pointer;
+    border-radius:50%;
+    transform:translateY(-50%);
+}
+.slider-wrapper .slider-button#prev-slide {
+    left:-20px;
+}
+.slider-wrapper .slider-button#next-slide {
+    right:-20px;
+}
+.slider-wrapper .image-list {
+    display:grid;
+    gap:18px;
+    font-size:0;
+    overflow-x:auto;
+    scrollbar-width:none;
+    grid-template-columns:repeat(10, 1fr);
+    margin-bottom:30px;
+}
+.slider-wrapper .image-list::-webkit-scrollbar {
+    display:none;
+}
+
+.slider-wrapper .image-list .image-item{
+    width:325px;
+    height:400px;
+    object-fit:cover;
+}
+.container .slider-scrollbar {
+    height:24px;
+    width:100%;
+    display:flex;
+    align-items:center;
+}
+.slider-scrollbar .scrollbar-track {
+    height:2px;
+    width:100%;
+    background:#ccc;
+    position: relative;
+    border-radius:4px;
+}
+.slider-scrollbar:hover .scrollbar-track {
+    height:4px;
+}
+.slider-scrollbar .scrollbar-thumb {
+    position:absolute;
+    height:100%;
+    width:50%;
+    background:#000;
+    border-radius: inherit;
+    cursor: grab;
+}
+.slider-scrollbar .scrollbar-thumb:active {
+    cursor:grabbing;
+    height:8px;
+    top: -2px;
+}
+.slider-scrollbar .scrollbar-thumb::after {
+    content:"";
+    position:absolute;
+    left:0;
+    right:0;
+    top:-10px;
+    bottom:-10px;
+}
+</style>
